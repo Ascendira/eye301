b455d4a1e27e9aae9000a7949343f58158c3a5e7
Liang Jincheng <112840576+Ascendira@users.noreply.github.com>
2025-02-10 13:16:36 +0800

    Add files via upload

---
+++ eye301_font/eye301/eye301/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for eye301 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+import patientInfo.views
+
+app_name = 'patientInfo'
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("basicInfo/add/<str:date>",patientInfo.views.BaseInfo.as_view()),
+    path("basicInfo/get/<int:pageNum>/<int:pageSize>",patientInfo.views.BaseInfo.as_view()),
+    path("patientInfo/post/<str:date>",patientInfo.views.PatientInfoView.as_view()),
+    path("patientInfo/get/<int:pageNum>/<int:pageSize>",patientInfo.views.PatientInfos.as_view()),
+    path("patientInfo/get/<str:patient_id>",patientInfo.views.PatientInfoView.as_view()),
+    path("patientInfo/get/base/<str:patient_id>", patientInfo.views.PatientInfoView.as_view()),
+    path("patientInfo/delete/<str:patient_id>", patientInfo.views.PatientInfoView.as_view()),
+]
---
+++ eye301_font/eye301/note.md
@@ -0,0 +1,9 @@
+1. Django项目开始接触
+
+[【一文到底】【0基础】【快速上手】Django基本使用-CSDN博客](https://blog.csdn.net/Ans_min/article/details/123146335?ops_request_misc=%7B%22request%5Fid%22%3A%222cc860100bfb229e517dc785ad7cf03d%22%2C%22scm%22%3A%2220140713.130102334..%22%7D&request_id=2cc860100bfb229e517dc785ad7cf03d&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-123146335-null-null.142^v101^pc_search_result_base7&utm_term=Django&spm=1018.2226.3001.4449)
+
+2. 直接使用DB Navigator查看数据库（测试数据库与本地数据库是分开的）
+
+3. 将Django项目部署到服务器上
+
+[Django项目部署到阿里云服务器（详细版）_django部署到阿里云-CSDN博客](https://blog.csdn.net/weixin_58293480/article/details/134741013)
